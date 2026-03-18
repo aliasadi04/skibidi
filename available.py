1bb92f0fdd9eebaae4063cbc8f30bb4b6fb66b4f
Ali <39883081+goorekt@users.noreply.github.com>
2026-03-18 15:34:40 +0100

    j

--- available.py
+++
@@ -1,3 +0,0 @@
-import torch
-torch.cuda.device(0)
-print(torch.cuda.is_available())
--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-#which gpus are available
-torch.cuda.device(1)    
-torch.cuda.get_device_name
-print(torch.cuda.is_available())
-print("********")
-print(torch.cuda.utilization(0))
-print(torch.cuda.utilization(1))
